835a674f9664027b00bb7b61e6efd8655c763fdc
Saulo <saulomiqueias7@gmail.com>
2025-07-19 18:51:43 -0300

    Create environment_config.dart

---
+++ modules/gtb_app/lib/env/environment_config.dart
@@ -0,0 +1,41 @@
+enum Environment {
+  dev,
+  uat,
+  prd;
+
+  factory Environment.fromValue(String value) {
+    return Environment.values.firstWhere(
+      (env) => env.name.toLowerCase() == value.toLowerCase(),
+      orElse: () => Environment.prd,
+    );
+  }
+}
+
+final class EnvironmentConfig {
+  final String baseUrl;
+  final Environment environment;
+
+  EnvironmentConfig({required this.baseUrl, this.environment = Environment.prd});
+
+  factory EnvironmentConfig.dev() => EnvironmentConfig(
+    baseUrl: 'https://6266f62263e0f382568936e4.dev.mockapi.io/',
+    environment: Environment.dev,
+  );
+  factory EnvironmentConfig.uat() => EnvironmentConfig(
+    baseUrl: 'https://6266f62263e0f382568936e4.uat.mockapi.io/',
+    environment: Environment.dev,
+  );
+  factory EnvironmentConfig.prd() =>
+      EnvironmentConfig(baseUrl: 'https://6266f62263e0f382568936e4.mockapi.io/');
+
+  EnvironmentConfig get config {
+    final envString = String.fromEnvironment('ENV', defaultValue: Environment.prd.name);
+    final env = Environment.fromValue(envString);
+
+    return switch (env) {
+      Environment.dev => EnvironmentConfig.dev(),
+      Environment.uat => EnvironmentConfig.uat(),
+      Environment.prd => EnvironmentConfig.prd(),
+    };
+  }
+}
