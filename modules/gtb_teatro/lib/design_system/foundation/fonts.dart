dedd8808b3f32995a5fc2fb3569bc14ee6e09490
Saulo <saulomiqueias7@gmail.com>
2026-01-31 01:25:01 -0300

    feat: added icons && fonts

--- modules/gtb_teatro/lib/design_system/foundation/fonts.dart
+++ modules/gtb_teatro/lib/design_system/foundation/fonts.dart
@@ -1,3 +1,4 @@
 abstract final class FontFamily {
-  static const String fontFamily = 'inter';
+  static const String fontFamily = 'BtgPactualSans';
+  static const String icons = 'OrquestraIcons';
 }
